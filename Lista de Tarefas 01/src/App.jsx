589f0ffcd1d5cfc50b382c2b3d65d3c08c36cefc
MarcosTsujino <marcos-tsu_jino@hotmail.com>
2024-03-07 16:26:11 -0300

    Adicionada uma variável de estado React chamada `todos` para gerenciar uma lista de itens de tarefas.

--- Lista de Tarefas 01/src/App.jsx
+++ Lista de Tarefas 01/src/App.jsx
@@ -1,8 +1,29 @@
 import { useState } from 'react'
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar funcionalidade X no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Ir pra Academia",
+      category: "Pessoal",
+      isCompleted: false
+    },
+    {
+      id: 3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false
+    },
+  ]);
+
   return (
-    <div>Olá React!</div>
+    <div className='app'>Olá React!</div>
   )
 }
 
