7b6de97c43ba1384331da4c59cc0c015477dddc8
MarcosTsujino <marcos-tsu_jino@hotmail.com>
2024-03-08 08:33:53 -0300

    Adicionar categoria aos itens de tarefas

--- Lista de Tarefas 01/src/App.jsx
+++ Lista de Tarefas 01/src/App.jsx
@@ -29,7 +29,10 @@ function App() {
         {todos.map((todo) => (
           <div className="todo">
               <div className="content">
-                <p>{todo.text}</p> `${todo.text}`
+                <p>{todo.text}</p>
+                <p className="category">
+                  ({todo.category})
+                </p>
               </div>
           </div>
         ))}
