eefd85bba3d2c2456f1f2890bf31a61d176b88d8
MarcosTsujino <marcos-tsu_jino@hotmail.com>
2024-03-15 08:58:11 -0300

    Adicionados estilos para parágrafos e entradas.

--- Lista de Tarefas 01/src/App.jsx
+++ Lista de Tarefas 01/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState } from "react";
+import './App.css'
 
 function App() {
   const [todos, setTodos] = useState([
@@ -12,33 +13,35 @@ function App() {
       id: 2,
       text: "Ir pra Academia",
       category: "Pessoal",
-      isCompleted: false
+      isCompleted: false,
     },
     {
       id: 3,
       text: "Estudar React",
       category: "Estudos",
-      isCompleted: false
+      isCompleted: false,
     },
   ]);
 
   return (
-    <div className='app'>
+    <div className="app">
       <h1>Lista de Tarefas</h1>
       <div className="todo-list">
         {todos.map((todo) => (
           <div className="todo">
-              <div className="content">
-                <p>{todo.text}</p>
-                <p className="category">
-                  ({todo.category})
-                </p>
-              </div>
+            <div className="content">
+              <p>{todo.text}</p>
+              <p className="category">({todo.category})</p>
+            </div>
+            <div>
+              <button>Completar</button>
+              <button>x</button>
+            </div>
           </div>
         ))}
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
